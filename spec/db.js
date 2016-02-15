045bf106c5d3b46539b0bb09807693a2ead23777
Kevin Anthoney <kevin@kanthoney.co.uk>
2016-02-15 19:57:28 +0000

    Add mysql test

--- spec/db.js
+++ spec/db.js
@@ -1,5 +1,14 @@
 
 'use strict';
 
-module.exports = require('knex')({ client: 'sqlite3', connection:{ filename: 'test.sqlite' }, useNullAsDefault: true });
+var config = {
+  'sqlite3': { client: 'sqlite3', connection:{ filename: 'test.sqlite' }, useNullAsDefault: true },
+  'mysql': { client: 'mysql',
+             connection: {
+               host: 'localhost',
+               user: 'kevin',
+               database: 'knex_users_test' } }
+}
+
+module.exports = require('knex')(config.mysql);
 
