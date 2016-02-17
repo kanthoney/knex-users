e3f40d40429c99b6dd44dc5215d51a2ac21a84df
Kevin Anthoney <kevin@kanthoney.co.uk>
2016-02-17 21:18:00 +0000

    Add changes which were left off last commit

--- spec/db.js
+++ spec/db.js
@@ -2,12 +2,14 @@
 'use strict';
 
 var config = {
-  'sqlite3': { client: 'sqlite3', connection:{ filename: 'test.sqlite' }, useNullAsDefault: true },
-  'mysql': { client: 'mysql',
-             connection: {
-               host: 'localhost',
-               user: 'kevin',
-               database: 'knex_users_test' } }
+  sqlite3: { client: 'sqlite3', connection:{ filename: 'test.sqlite' }, useNullAsDefault: true },
+  mysql: { client: 'mysql',
+           connection: {
+             host: 'localhost',
+             user: 'kevin',
+             database: 'knex_users_test' } },
+  pg: { client: 'pg',
+        connection: "postgresql://knex_users_test:test@localhost/knex_users_test" }
 }
 
 module.exports = require('knex')(config.sqlite3);
